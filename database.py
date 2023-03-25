b7e4beb3df2497cbb14efc9504076b0b94d97f0c
Rohan Mahesh Patil <46435460+marksindustries@users.noreply.github.com>
2023-03-25 08:39:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./ownwait.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://gmeiglph:wVc6XfBDD8S2AvoV1WZAtf7GQYNFA20c@satao.db.elephantsql.com/gmeiglph"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
